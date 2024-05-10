e58676b7a42fe905b5c845de4886cc65182ca6ba
MadeAditya02 <imadeaditya4@gmail.com>
2024-05-10 23:14:30 +0800

    Fix error 2

--- server/api/auth.post.ts
+++ server/api/auth.post.ts
@@ -1,8 +1,10 @@
 export default defineEventHandler(async (event) => {
   const {token} = await readBody(event);
+  console.log(token);
   await setUserSession(event, {
     user: {
-      token
+      token,
+      password: 'Hello'
     }
     // Any extra fields
   })
